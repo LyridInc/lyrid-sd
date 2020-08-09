32e9bd221707496d8873239c29ab9905f74b82e3
Handoyo Sutanto <hsutanto@lyrid.io>
2020-08-09 08:43:38 -0700

    removing some more logs

--- manager/manager.go
+++ manager/manager.go
@@ -46,7 +46,7 @@ func (manager *NodeManager) Init() {
 func (manager *NodeManager) ReRoute() {
 	// Close created route
 	log.Println("Re route")
-	for _, r  := range manager.RouteMap {
+	for _, r := range manager.RouteMap {
 		r.Close()
 		r = nil
 	}
@@ -81,7 +81,6 @@ func (manager *NodeManager) Run(ctx context.Context) {
 	config := model.GetConfig()
 	duration, _ := time.ParseDuration(config.Discovery_Poll_Interval)
 	for c := time.Tick(duration); ; {
-		log.Println("Polling Lyrid For new Info")
 		var sd []customSD
 		if len(manager.RouteMap) == 0 {
 			// first run, check old used ports on config file
@@ -107,7 +106,7 @@ func (manager *NodeManager) Run(ctx context.Context) {
 					if port != 0 {
 						r.Initialize(strconv.Itoa(port))
 					} else {
-						for ok := true; ok; ok = isReserved(port ,sd) {
+						for ok := true; ok; ok = isReserved(port, sd) {
 							port = manager.NextPortAvailable
 							manager.NextPortAvailable++
 						}
@@ -139,36 +138,34 @@ func (manager *NodeManager) GetExporterList() []model.ExporterEndpoint {
 	exporter_list := make([]model.ExporterEndpoint, 0)
 	response, err := sdk.GetInstance().ExecuteFunction(os.Getenv("FUNCTION_ID"), "LYR", utils.JsonEncode(model.LyFnInputParams{Command: "ListExporter"}))
 	if err != nil {
-		log.Println("error: ",err)
-	} else {
-		log.Println("response: ",string(response))
+		log.Println("error: ", err)
 	}
 	var jsonresp map[string]interface{}
 	json.Unmarshal([]byte(response), &jsonresp)
 	/*
-	exporter_list := make([]model.ExporterEndpoint, 0)
-	url := "http://localhost:8080"
+		exporter_list := make([]model.ExporterEndpoint, 0)
+		url := "http://localhost:8080"
 
-	request := make(map[string]interface{})
-	request["Command"] = "ListExporter"
+		request := make(map[string]interface{})
+		request["Command"] = "ListExporter"
 
-	jsonreq, _ := json.Marshal(request)
-	fmt.Println()
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonreq))
-	req.Header.Add("content-type", "application/json")
-	response, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return exporter_list
-	}
+		jsonreq, _ := json.Marshal(request)
+		fmt.Println()
+		req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonreq))
+		req.Header.Add("content-type", "application/json")
+		response, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return exporter_list
+		}
 
-	body, _ := ioutil.ReadAll(response.Body)
-	defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+		defer response.Body.Close()
 
-	var jsonresp map[string]interface{}
+		var jsonresp map[string]interface{}
 
-	json.Unmarshal(body, &jsonresp)
+		json.Unmarshal(body, &jsonresp)
 
-	 */
+	*/
 
 	if jsonresp["ReturnPayload"] != nil {
 		exporters_raw := jsonresp["ReturnPayload"].([]interface{})
